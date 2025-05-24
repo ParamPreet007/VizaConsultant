af166bef47a0576feee9819c88c3d2b0974ad681
Param <paramkhatridev007@gmail.com>
2025-05-24 13:46:28 +0530

    working on setup of tailwind css

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,16 @@ function App() {
   //   document.addEventListener("contextmenu", (event) => event.preventDefault())
   // }, [])
   return (
-    <Routes>
-      <Route path="/login" element={<Login />} />
-      <Route path="/forget-password" element={<AuthForgot />} />
-      <Route path="*" element={<AppLayout />} />
-      <Route path="/errorPage" element={<ErrorPage />} />
-    </Routes>
+    // <Routes>
+    //   <Route path="/login" element={<Login />} />
+    //   <Route path="/forget-password" element={<AuthForgot />} />
+    //   <Route path="*" element={<AppLayout />} />
+    //   <Route path="/errorPage" element={<ErrorPage />} />
+    // </Routes>
+    <div className="text-xl font-bold text-blue-600">
+
+      adsf
+    </div>
   );
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from "react"
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import "./components/common/antd-overridden.css";
+import "./component/common/antd-overridden.css"
 import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
